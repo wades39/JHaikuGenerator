1c87f505fbd91b4824f12a5fdf06518658abc5bc
wades39 <35578438+wades39@users.noreply.github.com>
2019-11-06 14:21:32 -0700

    Add files via upload

---
+++ src/generator/SyllableGetter.java
@@ -0,0 +1,94 @@
+package generator;
+
+/**
+ * This class acts as an object which will reference external sources (i.e.
+ * known web sites) in the goal of obtaining the number of syllables contained
+ * within a given word, and handled under a specified, supported language.
+ * 
+ * @author Kyler McMullin
+ *
+ */
+public class SyllableGetter {
+
+	private String language;
+
+	/**
+	 * Constructs the syllableGetter object
+	 */
+	public SyllableGetter(String language) {
+		this.language = language;
+	}
+
+	/**
+	 * Changes the language associated with the SyllableGetter object
+	 * 
+	 * @param language
+	 */
+	public void changeLanguage(String language) {
+		this.language = language;
+	}
+
+	/*
+	 * I could write specific algorithms which will get the number of syllables from
+	 * words in their respective languages, but that would've required a lot of
+	 * work.
+	 * 
+	 * Instead, I will be relying upon the Internet to get the syllable count for
+	 * each word.
+	 */
+
+	/**
+	 * Returns the number of syllables in a given token
+	 * 
+	 * @param token
+	 * @return syllableCount
+	 */
+	public int getSyllables(String token) {
+		switch (language) {
+		// if the current language is English, it will call to a sub-
+		// method which will delegate the process of getting syllables
+		// for an English word
+		case "English":
+			return getEnglishSyllables(token);
+		// same as above, but for Spanish
+		case "Spanish":
+			return getSpanishSyllables(token);
+		// still the same, just for French
+		case "French":
+			return getFrenchSyllables(token);
+		default:
+			throw new IllegalArgumentException("Language not yet supported!");
+		}
+	}
+
+	/**
+	 * Returns the number of syllables in an English token
+	 * 
+	 * @param token
+	 * @return syllableCount
+	 */
+	private int getEnglishSyllables(String token) {
+		return 0;
+	}
+
+	/**
+	 * Returns the number of syllables in a Spanish token
+	 * 
+	 * @param token
+	 * @return syllableCount
+	 */
+	private int getSpanishSyllables(String token) {
+		return 0;
+	}
+
+	/**
+	 * Returns the number of syllables in a French token
+	 * 
+	 * @param token
+	 * @return syllableCount
+	 */
+	private int getFrenchSyllables(String token) {
+		return 0;
+	}
+
+}
